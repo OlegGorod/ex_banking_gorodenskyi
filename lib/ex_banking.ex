87e423245c2250af869fde535fb313d7a97614f8
Oleg Gorodenskyi <ogorodenskyi.laba@solvd.com>
2024-07-25 00:28:06 +0300

    added functions get_balance and deposit

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -8,6 +8,29 @@ defmodule ExBanking do
   end
 
   def create_user(user) do
-    UserSupervisor.add_user(user)
+    with [] <- Registry.lookup(Registry.ExBanking, user) do
+      UserSupervisor.add_user(user)
+      :ok
+    else
+      [{_user_pid, _state}] -> {:error, :user_already_exists}
+    end
+  end
+
+  def get_balance(user) do
+    with [{user_pid, _value}] <- Registry.lookup(Registry.ExBanking, user),
+         new_balance <- GenServer.call(user_pid, :get_balance) do
+      new_balance
+    else
+      [] -> {:error, :user_does_not_exist}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  def deposit(user, amount, currency) do
+    with [{user_pid, _value}] <- Registry.lookup(Registry.ExBanking, user) do
+      GenServer.call(user_pid, {:deposit, amount, currency})
+    else
+      [] -> {:error, :user_not_found}
+    end
   end
 end
--- lib/user.ex
+++ lib/user.ex
@@ -2,10 +2,20 @@ defmodule ExBanking.User do
   use GenServer
 
   def start_link(user_name) do
-    GenServer.start_link(__MODULE__, user_name, name: {:via, Registry, {Registry.ExBanking, user_name}})
+    GenServer.start_link(__MODULE__, %{}, name: {:via, Registry, {Registry.ExBanking, user_name}})
   end
 
   def init(state) do
     {:ok, state}
   end
+
+  def handle_call(:get_balance, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_call({:deposit, amount, currency}, _, state) do
+    new_state = Map.update(state,currency,amount, fn value -> value + amount end)
+    {:reply, new_state, new_state}
+  end
+
 end
